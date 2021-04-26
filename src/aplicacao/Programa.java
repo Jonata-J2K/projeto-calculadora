d21d9de52435214eb639e04fdb62584275d7022f
Jonata Sousa <devjonataj2k@gmail.com>
2021-04-26 11:09:25 -0300

    Feito localização e Scanner

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,10 +1,16 @@
 package aplicacao;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		sc.close();
 	}
 
 }
