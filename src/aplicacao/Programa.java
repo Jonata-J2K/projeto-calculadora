c5f6b986f8fde2c7a297aadd444a7306df76ac0d
Jonata Sousa <devjonataj2k@gmail.com>
2021-04-26 11:15:24 -0300

    Feito operações de entrada de dados

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -10,6 +10,14 @@ public class Programa {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+		System.out.print("Informe o primeiro numero: ");
+		double numero1 = sc.nextDouble();
+		System.out.print("Informe a operação (+,-,/,*)");
+		String operacao = sc.next();
+		System.out.print("Informe outro numero: ");
+		double numero2 = sc.nextDouble();
+		
+		
 		sc.close();
 	}
 
